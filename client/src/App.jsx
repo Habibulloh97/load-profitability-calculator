bc4b9711de7b81d82798b3bd555a0e3b11dd52d7
Habibulloh97 <81605677+Habibulloh97@users.noreply.github.com>
2026-05-03 14:31:49 +0500

    Day 1: project init - frontend shell + backend with Mongo Atlas

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button"
+function App(){
+  return(
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind Works</h1>
+      <Button>Hello</Button>
+    </div>
+)}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors({
+    origin : process.env.CLIENT_URL,
+    credentials: true
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.get("/health", (req,res)=>{
+    res.json({ok:true, ts: new Date().toDateString()})
+})
+
+const PORT =process.env.PORT ||"5000"
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log("Mongo Connected"))
+    .catch((err)=>console.error("Mongo Connection Failed:", err.message))
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
