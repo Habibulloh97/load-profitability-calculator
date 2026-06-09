f47713388aa47974b728d5f94f130065b37ca67b
Habibulloh97 <81605677+Habibulloh97@users.noreply.github.com>
2026-06-09 05:09:38 +0500

    fix: global polyfill for process.env in production

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -11,6 +12,14 @@ export default defineConfig({
     },
   },
   define: {
-    "process.env": {},
+    // This provides a global safety fallback for third-party scripts expecting Node environments
+    "process.env": {
+      NODE_ENV: JSON.stringify("production"),
+    },
+    "global.process": {
+      env: {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    },
   },
 });
